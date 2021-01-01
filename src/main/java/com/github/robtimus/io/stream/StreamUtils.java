27b66fa3fb4ff3183a8b93a46a4801bff7cea320
Rob Spoor <rob@icemanx.nl>
2021-01-01 16:28:23 +0100

    Removed StreamUtils.dontClose methods as commons-io 2.9.0 will add CloseShield*.wrap methods that do the same

--- src/main/java/com/github/robtimus/io/stream/StreamUtils.java
+++ src/main/java/com/github/robtimus/io/stream/StreamUtils.java
@@ -22,11 +22,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
-import java.util.Objects;
-import org.apache.commons.io.input.CloseShieldInputStream;
-import org.apache.commons.io.input.CloseShieldReader;
-import org.apache.commons.io.output.CloseShieldOutputStream;
-import org.apache.commons.io.output.CloseShieldWriter;
 
 /**
  * Utility methods for {@link InputStream InputStreams}, {@link OutputStream OutputStreams}, {@link Reader Readers} and {@link Writer Writers}.
@@ -39,104 +34,6 @@ public final class StreamUtils {
 
     private StreamUtils() {
         throw new IllegalStateException("cannot create instances of " + getClass().getName()); //$NON-NLS-1$
-    }
-
-    // wrapping
-
-    /**
-     * Wraps an input stream to prevent it from being closed.
-     * This method can be used when a method wants to close a passed input stream while the input stream is still needed.
-     * <p>
-     * This method is much like {@link CloseShieldInputStream}. However, unlike that class, IDEs can still show a warning if the wrapped input stream
-     * is not closed elsewhere. This can prevent accidentally forgetting to close input streams completely. For instance:
-     * <pre><code>
-     * try (InputStream input = new CloseShieldInputStream(new FileInputStream(file))) { // no warning
-     * }
-     * try (InputStream input = dontClose(new FileInputStream(file))) { // warning
-     * }
-     * </code></pre>
-     *
-     * @param input The input stream to wrap.
-     * @return An input stream wrapper around the given input stream that will delegate all methods except for {@code close()}.
-     * @throws NullPointerException If the given input stream is {@code null}.
-     */
-    @SuppressWarnings("resource")
-    public static InputStream dontClose(InputStream input) {
-        Objects.requireNonNull(input);
-        return new CloseShieldInputStream(input);
-    }
-
-    /**
-     * Wraps an output stream to prevent it from being closed.
-     * This method can be used when a method wants to close a passed output stream while the output stream is still needed.
-     * Another usage is in a try-with-resources block where a wrapping output stream needs to be closed to finish its work, but the wrapped output
-     * stream should still remain open.
-     * <p>
-     * This method is much like {@link CloseShieldOutputStream}. However, unlike that class, IDEs can still show a warning if the wrapped output
-     * stream is not closed elsewhere. This can prevent accidentally forgetting to close output streams completely. For instance:
-     * <pre><code>
-     * try (OutputStream output = new CloseShieldOutputStream(new FileOutputStream(file))) { // no warning
-     * }
-     * try (OutputStream output = dontClose(new FileOutputStream(file))) { // warning
-     * }
-     * </code></pre>
-     *
-     * @param output The output stream to wrap.
-     * @return An output stream wrapper around the given output stream that will delegate all methods except for {@code close()}.
-     * @throws NullPointerException If the given output stream is {@code null}.
-     */
-    @SuppressWarnings("resource")
-    public static OutputStream dontClose(OutputStream output) {
-        Objects.requireNonNull(output);
-        return new CloseShieldOutputStream(output);
-    }
-
-    /**
-     * Wraps a reader to prevent it from being closed.
-     * This method can be used when a method wants to close a passed reader while the reader is still needed.
-     * <p>
-     * This method is much like {@link CloseShieldReader}. However, unlike that class, IDEs can still show a warning if the wrapped reader is not
-     * closed elsewhere. This can prevent accidentally forgetting to close readers completely. For instance:
-     * <pre><code>
-     * try (Reader input = new CloseShieldReader(new FileReader(file))) { // no warning
-     * }
-     * try (Reader input = dontClose(new FileReader(file))) { // warning
-     * }
-     * </code></pre>
-     *
-     * @param input The reader to wrap.
-     * @return A reader wrapper around the given reader that will delegate all methods except for {@code close()}.
-     * @throws NullPointerException If the given reader is {@code null}.
-     */
-    @SuppressWarnings("resource")
-    public static Reader dontClose(Reader input) {
-        Objects.requireNonNull(input);
-        return new CloseShieldReader(input);
-    }
-
-    /**
-     * Wraps a writer to prevent it from being closed.
-     * This method can be used when a method wants to close a passed writer while the writer is still needed.
-     * Another usage is in a try-with-resources block where a wrapping writer needs to be closed to finish its work, but the wrapped writer should
-     * still remain open.
-     * <p>
-     * This method is much like {@link CloseShieldWriter}. However, unlike that class, IDEs can still show a warning if the wrapped writer is not
-     * closed elsewhere. This can prevent accidentally forgetting to close writers completely. For instance:
-     * <pre><code>
-     * try (Writer output = new CloseShieldWriter(new FileWriter(file))) { // no warning
-     * }
-     * try (Writer output = dontClose(new FileWriter(file))) { // warning
-     * }
-     * </code></pre>
-     *
-     * @param output The writer to wrap.
-     * @return A writer wrapper around the given writer that will delegate all methods except for {@code close()}.
-     * @throws NullPointerException If the given writer is {@code null}.
-     */
-    @SuppressWarnings("resource")
-    public static Writer dontClose(Writer output) {
-        Objects.requireNonNull(output);
-        return new CloseShieldWriter(output);
     }
 
     // index checking
